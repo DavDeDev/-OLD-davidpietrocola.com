768a00c395ba1d6e5ac6933c88fc545af9b9f714
DavDeDev <pietrocoladavid@gmail.com>
2023-03-06 23:19:35 -0500

    feat: scrolling-wheel

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,14 +14,33 @@ const Hero = () => {
           <div className="w-1 sm:h:80 h-40 violet-gradient" />
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span className="text-[#915eff]">David</span></h1>
+          <h1 className={`${styles.heroHeadText}`}>
+            Hi, I'm <span className="text-[#915eff]">David</span>
+          </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I'm a student and a developer. <br className="sm:block hidden"/> I'm passionate about building things
+            I'm a student and a developer. <br className="sm:block hidden" />{" "}
+            I'm passionate about building things
           </p>
         </div>
       </div>
 
-        <ComputersCanvas/>
+      <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -30,6 +30,8 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  
+
   return (
     <Canvas
       frameLoop="demand"
