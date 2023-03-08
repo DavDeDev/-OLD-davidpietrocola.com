5c47617df9f436c59c35917825c9bc2236bbafeb
DavDeDev <pietrocoladavid@gmail.com>
2023-03-08 07:11:45 -0500

    fix: hero left vertical

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
-        <div className="flex flex-col justify-center items-start mt-5">
+        <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h:80 h-40 violet-gradient" />
         </div>
