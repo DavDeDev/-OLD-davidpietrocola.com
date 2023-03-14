a6093b9863adcb6a38368ef4668354b506440926
DavDeDev <pietrocoladavid@gmail.com>
2023-03-13 22:31:17 -0400

    fix: about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a student and a developer. I'm passionate about building things.
+        I'm a student and a developer. I'm passionate about building things with
         experience in building websites and web applications. I'm currently
         learning<span className="blue-text-gradient"> React</span>, <span className="pink-text-gradient">Blockchain development</span>, and <span className="green-text-gradient">Testing practices</span>.
       </motion.p>
