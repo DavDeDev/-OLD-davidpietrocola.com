642518ac1978f25b1e612baa73f05613e063f5f3
DavDeDev <pietrocoladavid@gmail.com>
2023-07-14 22:42:27 -0400

    test: CI/CD

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ const App = () => {
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
-          <Hero />
         </div>
         <About />
         <Experience />
